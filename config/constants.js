1c1bb5bc0649d9b116a4d24b4c50e423e58d0622
nitish-yadav-robomq <nitish.yadav@robomq.io>
2025-08-28 00:13:34 +0530

    updated codes

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+let movies = {
+    comics_odyssey  : {
+        name  : 'Cosmic Odyssey',
+        year : 2024,
+        genre : 'Sci-Fi',
+        rating : '8.7',
+    }
+}
--- js/index.js
+++ js/index.js
@@ -9,8 +9,6 @@
 // let buttons = ["Home","Movies","Tv Shows","My List"];
 // addButtons(buttons);
 
-
-
 let homeButton = document.querySelector('.home');
 homeButton.addEventListener('click', (e) => {
     window.location.href = 'index.html';
@@ -28,8 +26,28 @@ myListButton.addEventListener('click', (e) => {
     window.location.href = 'mylist.html';
 })
 
-let imageCard = document.querySelector('.image-card');
-console.log("imageCard",imageCard);
-imageCard.addEventListener('hover', (e) => {
-    console.log("fgchvjbknlm")
+let imageCards = document.querySelectorAll('.image-card');
+imageCards.forEach((imageCard) => {
+    let movieDetailElement = imageCard.querySelector('.movie-details');
+    console.log("movie details", movieDetailElement);
+    console.log("image card", imageCard);
+    imageCard.addEventListener('mouseenter', (e) => {
+        console.log("movie enter");
+        movieDetailElement.style.display = 'block';
+    })
+    imageCard.addEventListener('mouseleave', (e) => {
+        console.log("movie leave", );
+        movieDetailElement.style.display = 'none';
+    })
 })
+
+let allButtons = document.querySelectorAll('.add-to-list');
+allButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+       let movieKey = button.dataset.id;
+       localStorage.setItem('moviesKey', movieKey);
+    })
+})
+
+localStorage.removeItem('name')
+localStorage.removeItem('moviesKey')
